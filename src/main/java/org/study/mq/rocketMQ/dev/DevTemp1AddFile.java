56bc271a61fbfc28c855e7cefaab6bcd92c7ffd1
kingkong <kongq1983@qq.com>
2022-03-31 22:01:40 +0800

    add devtemp1

---
+++ src/main/java/org/study/mq/rocketMQ/dev/DevTemp1AddFile.java
@@ -0,0 +1,16 @@
+package org.study.mq.rocketMQ.dev;
+
+/**
+ * DevTempAddFile
+ *
+ * @author kq
+ * @date 2022-03-31 21:43
+ * @since 1.0.0
+ */
+public class DevTemp1AddFile {
+
+    public static void main(String[] args) {
+        System.out.println("DevTempAddFile");
+    }
+
+}
