66f4619052e6a8555f42a1380deac697fa339916
kingkong <kongq1983@qq.com>
2022-11-14 00:28:21 +0800

    add devtemp1

--- src/main/java/org/study/mq/rocketMQ/dev/FirstDevDemo.java
+++ src/main/java/org/study/mq/rocketMQ/dev/FirstDevDemo.java
@@ -12,6 +12,7 @@ public class FirstDevDemo {
     public static void main(String[] args) {
         System.out.println("this is first dev demo");
         System.out.println("this is 2022-11 dev demo");
+        System.out.println("this is 2022-11 dev1 demo");
     }
 
 }
