a1c455e65080075b9e14c9806fcf076aee16b7b0
kong <futong2016@163.com>
2023-10-23 13:43:44 +0800

    add DevVersion

---
+++ src/main/java/org/study/mq/rocketMQ/dev/DevVersion.java
@@ -0,0 +1,18 @@
+package org.study.mq.rocketMQ.dev;
+
+/**
+ * dev版本
+ * @author: kq
+ * @date: 2023-10-23 11:23:05
+ * @since: 1.0.0
+ * @description:
+ */
+public class DevVersion {
+
+    /**
+     * 主线版本 添加version:1
+     */
+    public static final Integer DEV1 = 1;
+
+
+}
